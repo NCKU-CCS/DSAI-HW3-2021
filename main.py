ca31d99b1b142d4afb62423bf5718e3f77cad5f4
Jhe-Yun <tizan0826@gmail.com>
2021-04-18 14:12:30 +0800

    fix: template file

---
+++ main.py
@@ -0,0 +1,29 @@
+
+# You should not modify this part.
+def config():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--consumption", default="./sample_data/consumption.csv", help="input the consumption data path")
+    parser.add_argument("--generation", default="./sample_data/generation.csv", help="input the generation data path")
+    parser.add_argument("--bidresult", default="./sample_data/bidresult.csv", help="input the bids result path")
+    parser.add_argument("--output", default="output.csv", help="output the bids path")
+
+    return parser.parse_args()
+
+
+def output(path, data):
+    import pandas as pd
+
+    df = pd.DataFrame(data, columns=["time", "action", "target_price", "target_volume"])
+    df.to_csv(path, index=False)
+
+    return
+
+
+if __name__ == "__main__":
+    args = config()
+
+    data = [["2018-01-01 00:00:00", "buy", 2.5, 3],
+            ["2018-01-01 01:00:00", "sell", 3, 5]]
+    output(args.output, data)
